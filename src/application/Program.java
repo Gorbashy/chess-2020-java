a5db5c98bdebb4e84ac229eac7cc09a55a125dfc
Gorbashy <james.freire@wapnet.com.br>
2020-07-08 21:17:20 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,9 +38,8 @@ public class Program {
 				
 				if (capturedPiece !=null) {
 					captured.add(capturedPiece);
-					
 				}
-			}
+				}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
